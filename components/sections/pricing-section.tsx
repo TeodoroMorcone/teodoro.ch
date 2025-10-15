ac4717fb59a121f23f3598e8794f0e40f524e907
Teodoro Morcone <tmorcone@outlook.com>
2025-10-15 12:37:51 +0200

    prices

--- components/sections/pricing-section.tsx
+++ components/sections/pricing-section.tsx
@@ -22,7 +22,7 @@ export function PricingSection({pricing}: PricingSectionProps) {
         }
       />
 
-      <div className="mt-10 grid gap-6 lg:grid-cols-3">
+      <div className="mt-10 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {pricing.plans.map((plan: PricingPlan) => (
           <article
             key={plan.id}
